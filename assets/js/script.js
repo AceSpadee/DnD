131978a9d65f068068d33fde8c9592be7536184e
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-05-12 23:44:48 -0700

    added in the rest of the styling and fixing bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
-// const test = document.getElementsByClassName("#header");
-
-// console.log(test)
-
-const h2Elements = document.querySelectorAll('h2');
-
-console.log(h2Elements)
+console.log("hello")
